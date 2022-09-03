ced5673940d05f216679169dead7ff08f4fbc6e5
TensegrityBlockxels <103687791+TensegrityBlockxels@users.noreply.github.com>
2022-09-03 14:09:33 -0300

    Completed the team component.

--- getStats.py
+++ getStats.py
@@ -3,8 +3,13 @@ import datetime
 isMonday = datetime.date.today().isoweekday() == 1
 def downloadGameStats(gameName):
     req = requests.get(f"https://api.mcchampionship.com/v1/halloffame/{gameName}")
-    with open(f"db/{gameName}.json", "w") as stat:
+    with open(f"db/team/{gameName}.json", "w") as stat:
         stat.write(req.text)
+
+def downloadParticipantStats(teamName):
+    team = requests.get(f'https://api.mcchampionship.com/v1/participants/{teamName}')
+    with open(f"db/participants{teamName}.json", "w") as teamw:
+        teamw.write(team.text)
 
 if True == True:
     rundown = requests.get("https://api.mcchampionship.com/v1/rundown")
@@ -12,22 +17,28 @@ if True == True:
         individualScores.write(rundown.text)
 
 
-    downloadGameStats("MG_ROCKET_SPLEEF")
-    downloadGameStats("MG_SURVIVAL_GAMES")
-    downloadGameStats("PARKOUR_WARRIOR")
-    downloadGameStats("MG_ACE_RACE")
-    downloadGameStats("MG_TGTTOSAWAF")
-    downloadGameStats("MG_SKYBATTLE")
-    downloadGameStats("MG_HOLE_IN_THE_WALL")
-    downloadGameStats("MG_BATTLE_BOX")
-    downloadGameStats("MG_BUILD_MART")
-    downloadGameStats("MG_SANDS_OF_TIME")
-    downloadGameStats("MG_DODGEBOLT")
-    downloadGameStats("MG_PARKOUR_TAG")
-    downloadGameStats("MG_GRID_RUNNERS")
-    downloadGameStats("MG_MELTDOWN")
+    # downloadGameStats("MG_ROCKET_SPLEEF")
+    # downloadGameStats("MG_SURVIVAL_GAMES")
+    # downloadGameStats("PARKOUR_WARRIOR")
+    # downloadGameStats("MG_ACE_RACE")
+    # downloadGameStats("MG_TGTTOSAWAF")
+    # downloadGameStats("MG_SKYBATTLE")
+    # downloadGameStats("MG_HOLE_IN_THE_WALL")
+    # downloadGameStats("MG_BATTLE_BOX")
+    # downloadGameStats("MG_BUILD_MART")
+    # downloadGameStats("MG_SANDS_OF_TIME")
+    # downloadGameStats("MG_DODGEBOLT")
+    # downloadGameStats("MG_PARKOUR_TAG")
+    # downloadGameStats("MG_GRID_RUNNERS")
+    # downloadGameStats("MG_MELTDOWN")
 
-
-    RED = requests.get('https://api.mcchampionship.com/v1/participants/RED')
-    with open("db/participantsRed.json", "w") as redr:
-        redr.write(RED.text)
+    downloadParticipantStats("RED")
+    downloadParticipantStats("ORANGE")
+    downloadParticipantStats("YELLOW")
+    downloadParticipantStats("GREEN")
+    downloadParticipantStats("LIME")
+    downloadParticipantStats("AQUA")
+    downloadParticipantStats("CYAN")
+    downloadParticipantStats("BLUE")
+    downloadParticipantStats("PURPLE")
+    downloadParticipantStats("PINK")
