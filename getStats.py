b16e4c8fa156810719f3683fea929c6267edf747
TensegrityBlockxels <103687791+TensegrityBlockxels@users.noreply.github.com>
2022-11-10 13:55:44 -0300

    FIxed issue of invalid scores, now appear as 0000

--- getStats.py
+++ getStats.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 import datetime, sys
 isMonday = datetime.date.today().isoweekday() == 1
 def downloadGameStats(gameName):
@@ -13,21 +13,42 @@ def downloadParticipantStats(teamName):
 
 
 def downloadSetup():
-    if isMonday == True or sys.argv[1] == "download":
-        rundown = requests.get("https://api.mcchampionship.com/v1/rundown")
-        with open("static/db/individualScores.json", "w") as individualScores:
-            individualScores.write(rundown.text)
+    if isMonday == True or sys.argv[1] == "download" or sys.argv[1] == "cache":
+        TEAMS = ["RED","ORANGE","YELLOW","GREEN","LIME","AQUA","CYAN","BLUE","PURPLE","PINK"]
 
-        downloadParticipantStats("RED")
-        downloadParticipantStats("ORANGE")
-        downloadParticipantStats("YELLOW")
-        downloadParticipantStats("GREEN")
-        downloadParticipantStats("LIME")
-        downloadParticipantStats("AQUA")
-        downloadParticipantStats("CYAN")
-        downloadParticipantStats("BLUE")
-        downloadParticipantStats("PURPLE")
-        downloadParticipantStats("PINK")
+        if sys.arv[1] == "cache":
+            rundown = requests.get("https://api.mcchampionship.com/v1/rundown")
+            with open("static/db/individualScores.json", "w") as individualScores:
+                individualScores.write(rundown.text)
+            for team in TEAMS:
+                downloadParticipantStats(f"{team}")
+
+        with open("static/db/individualScores.json", "r") as scoreReader:
+            content = json.loads(scoreReader.read())
+            scores = content["data"]["individualScores"]
+
+        participantList = []
+        for team in TEAMS:
+            with open(f"static/db/participants{team}.json", "r") as participantReader:
+                participants = json.loads(participantReader.read())
+                participants = participants["data"]       
+                for participant in range(len(participants)):
+                    participantList.append(participants[participant]["username"])
+
+
+    diff_names = list(set(participantList) - set(scores))
+
+    diff_name_dict = {diff: "0000" for diff in diff_names}
+    content_dict = content["data"]["individualScores"]
+    content_dict.update(diff_name_dict)
+    
+
+    content["data"]["individualScores"] = content_dict
+
+    # print(content)
+
+    with open("static/db/individualScores.json", "w") as scoreFixer:
+        scoreFixer.write(content)
 
 if sys.argv[1] == "download":
     downloadSetup()
